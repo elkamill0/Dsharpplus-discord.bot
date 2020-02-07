f82e761dbd236cb6bde222ce8cfbc6751cfcfc00
elkamill0 <58825229+elkamill0@users.noreply.github.com>
2020-02-07 20:20:34 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DSharpPlus.Net.Abstractions;
+using DSharpPlus.Net.WebSocket;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
+
+
+namespace bot_discord
+{
+    
+    public class Commands
+    {
+        static public string user_money;
+
+        [Command("join"), Description("Joins a voice channel.")]
+        public async Task Join(CommandContext ctx, DiscordChannel chn = null)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+            if (vnext == null)
+            {
+                await ctx.RespondAsync("VNext is not enabled or configured.");
+                return;
+            }
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc != null)
+            {
+                vnc = await vnext.ConnectAsync(chn);
+                await ctx.RespondAsync($"Already connected in this guild: `{chn.Name}`");
+                return;
+            }
+
+            var vstat = ctx.Member?.VoiceState;
+            if (vstat?.Channel == null && chn == null)
+            {
+                await ctx.RespondAsync("You are not in a voice channel.");
+                return;
+            }
+
+            if (chn == null)
+                chn = vstat.Channel;
+
+            vnc = await vnext.ConnectAsync(chn);
+            await ctx.RespondAsync($"Connected to `{chn.Name}`");
+        }
+
+        [Command("leave")]
+        public async Task Leave(CommandContext ctx)
+        {
+            var vnext = ctx.Client.GetVoiceNextClient();
+
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+                throw new InvalidOperationException("Not connected in this guild.");
+
+            vnc.Disconnect();
+            await ctx.RespondAsync("👌");
+        }
+
+        [Command("kanał_nazwa")]
+        public async Task Name_channel(CommandContext ctx, string n1, string n2 = null, string n3 = null, string n4 = null)
+        {
+            var chn = ctx.Member?.VoiceState;
+            if(chn?.Channel == null)
+            {
+                ctx.RespondAsync("Nie znajdujesz się na kanale głosowym (ctx.Member.Voicestate.Channel != null)");
+            }
+            string name = ($"{n1} {n2} {n3} {n4}");
+            chn.Channel.ModifyAsync(name);
+            ctx.RespondAsync($"Nazwa kanału '{ctx.Member.VoiceState.Channel.Name}' została pomyślnie zmieniona na '{name}'");
+
+
+        }
+
+        [Command("zbanuj")]
+        public async Task BanAsync(CommandContext ctx, DiscordMember member, int delete_message_days = 0, string reason = null)
+        {
+        //    ctx.Guild.BanMemberAsync(member, delete_message_days, reason);
+        //    await ctx.RespondAsync($"{member} pomyślnie zbanowany");
+        }
+
+        [Command("postac")]
+        public async Task Postac(CommandContext ctx, int row)
+        {
+            var rnd = new Random();
+            var rnd1 = new Random();
+            await ctx.RespondAsync($"🎲 Rząd: {rnd.Next(0, row)}");
+            await ctx.RespondAsync($"🎲 Postać: {rnd1.Next(0, 7)}");
+        }
+
+        [Command("miotaj")]
+        public async Task move(CommandContext ctx, DiscordMember member, DiscordChannel chn, DiscordChannel chn1, int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                member.PlaceInAsync(chn);
+                member.PlaceInAsync(chn1);
+            }
+            ctx.RespondAsync($"{ctx.Member.Mention}, Miotanie zakończone");
+        }
+
+        [Command("role")]
+        public async Task rola(CommandContext ctx, string name, Permissions? permissions,
+            DiscordColor? color, bool? hoist = null, bool? mentionable = null,
+            string reason = null)
+        {
+            ctx.Guild.CreateRoleAsync(name, permissions, color, hoist, mentionable, reason);
+        }
+
+        [Command("wakeup!")]
+        public async Task wakeup(CommandContext ctx, DiscordMember member, int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+                ctx.RespondAsync($"{member.Mention}");
+        }
+
+        [Command("ban")]
+        public async Task Ban(CommandContext ctx)
+        {
+            Random rng = new Random();
+            int random = rng.Next(0, 2);
+            if (random == 0)
+                await ctx.RespondAsync($"{ctx.User.Mention} czego");
+            else if (random == 1)
+                await ctx.RespondAsync($"{ctx.User.Mention} dostajesz bana, ty kurwo jebana!");
+        }
+
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"Witaj Towarzyszu {ctx.User.Mention}");
+        }
+
+        [Command("random")]
+        public async Task Random(CommandContext ctx, int min, int max)
+        {
+            var rnd = new Random();
+            await ctx.RespondAsync($"🎲 Wylosowana liczba to: {rnd.Next(min, max)}");
+        }
+
+        [Command("rola")]
+        public async Task Role(CommandContext ctx, DiscordRole role = null)
+        {
+            string name = "Prawa ręka boga";
+            DiscordMember member = ctx.Member;
+            await ctx.Guild.GrantRoleAsync(member, role, reason: null);
+        }
+
+        [Command("hajs")]
+        public async Task Money(CommandContext ctx)
+        {
+            user_money = ctx.Member.Username;
+            Talony.StreamReader();
+            await ctx.RespondAsync($"Stan konta {user_money} wynosi: {Talony.i_money}");
+        }
+
+        [Command("dodaj_hajs")]
+        public async Task AddMoney(CommandContext ctx, int money)
+        {
+            user_money = ctx.Member.Username;
+            Talony.StreamReader();
+            Talony.i_money = Talony.i_money + money;
+            Talony.StreamWriter();
+            await ctx.RespondAsync($"Dodano {money} dla użytkownika {user_money}, {Talony.i_money} ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DSharpPlus.Net.Abstractions;
+using DSharpPlus.Net.WebSocket;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.VoiceNext;
+
+namespace bot_discord
+{
+    public class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+        static VoiceNextClient voice;
+        static DiscordApplication app;
+        static DiscordUser usr;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "private",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "@Kirito Senpai#3481 "
+            });
+
+            discord.MessageCreated += async e =>
+            {
+
+                if (e.Message.Content.ToLower().Contains("kurwa") ||
+                    e.Message.Content.ToLower().Contains("jebać") ||
+                    e.Message.Content.ToLower().Contains("huj"))
+                {
+                    await e.Message.RespondAsync($"{e.Author.Mention}, nie przeklinaj!");
+                //    await e.Message.RespondAsync($"Abyś nauczył się dobrych manier (zanim dostaniesz bana) zostanie odebrane Ci 200 szt. Kiri-talonów");
+                    
+                }
+            };
+
+            discord.MessageCreated += async e =>
+            {
+                string reason = null;
+                if (e.Message.Content.ToLower().StartsWith("!") && e.Message.Channel.Name != "kanał-do-bota-muzycznego-d")
+                {
+                    await e.Message.RespondAsync($"{e.Author.Mention}, to nie jest kanał do bota muzycznego!");
+                    e.Message.DeleteAsync(reason);
+                }
+            };
+
+
+
+
+            discord.MessageCreated += async e =>
+            {
+                string reason = null;
+                if (!e.Message.Content.Contains("!") && e.Message.Channel.Name == ("kanał-do-bota-muzycznego-d"))
+                {
+                    CommandContext ctx = null;
+                    string content = ($"Warn{e.Message.Author.Mention}, pisanie na kanale do bota muzycznego");
+
+                    if (!e.Message.Author.IsBot)
+                    {
+                        e.Message.DeleteAsync(reason);
+                        e.Message.RespondAsync($"Warn {e.Message.Author.Mention}, pisanie na kanale do bota muzycznego!");
+                    }
+                    var chn = e.Channel;
+                }
+
+            };
+
+            discord.GuildMemberAdded += async e =>
+            {
+                CommandContext ctx = null;
+                var vnext = ctx.Client.GetVoiceNextClient();
+                var chn_name = ctx.Channel.Name;
+
+                chn_name = "Gry #1";
+                string name = "Gry #2";
+                var user = ctx.Client.GetVoiceNextClient();
+                //  var vstat = ctx.Member?.VoiceState;
+
+                if ((e.Member.VoiceState.Guild.Name == chn_name) != null)
+                {
+                    e.Guild.CreateChannelAsync(name, type: ChannelType.Voice, parent: null, bitrate: null, user_limit: null, overwrites: null, reason: null);
+                }
+            };
+
+            discord.GuildMemberAdded += async e =>
+            {
+
+                CommandContext ctx = null;
+                DiscordEmbed embed;
+                bool is_tts;
+                string content = $"Witaj w Rosyjskim Towarzystwie.";
+                await e.Member.SendMessageAsync(content, is_tts = false, embed = null);
+
+                //  string name = e.Member.name;
+
+                DiscordMember member = e.Member;
+                DiscordRole role;
+                await e.Guild.GrantRoleAsync(member, role = null, reason: null);
+
+
+            };
+
+            voice = discord.UseVoiceNext();
+            commands.RegisterCommands<Commands>();
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+    }
+
+}
